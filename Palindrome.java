0682e527c34a0d470f3fe7e4290dfd3ff297916a
Parag-Bhoyar <paragbhoyar1607@gmail.com>
2026-02-03 23:04:57 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,20 @@
+package allasign;
+
+public class Palindrome {
+	public static void main(String[] args) {
+	System.out.println(checkPalinIgnoringCase("madam"));
+}
+public static boolean checkPalinIgnoringCase(String str) {
+	int left = 0;
+	int right = str.length()-1;
+	
+	while(left<right) {
+		if(str.charAt(left)!= str.charAt(right)) {
+			return false;
+		}
+		left ++;
+		right --;
+	}
+	return true;
+}
+}
